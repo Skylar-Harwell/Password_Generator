9f76b85d9e540d445bb8302bb3895d23838f7bc7
Skylar Harwell <swharwell@gmail.com>
2021-07-11 15:50:43 -0400

    Additional styling and Java coding.

--- assets/script.js
+++ assets/script.js
@@ -12,29 +12,38 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-var lower = 'abcdefghijklmnopqrstuvwxyz';
-var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var num = '0123456789';
+// Created Variabkes for password creation
+var lower = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
+var upper = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
+var num = '0,1,2,3,4,5,6,7,8,9';
 var sym = '!#$%&*()=-+/{}';
-var possChar = [];
-var selChar = [];
-    
+
 function generatePassword() {
+    //Initial Prompt for password length when button clicked
     let passwordLength = window.prompt('Please chose the length of your password between 8 and 128.');
     
+    //User defined password lenght defined
     passwordLength = parseInt(passwordLength);
     console.log(typeof passwordLength);
 
-    if(passwordLength >= 8 && passwordLength <= 128) {
-        console.log(passwordLength);
-    } else {
-        alert("Password must be at least 8 characters.");
-    }
+    // if(passwordLength >= 8 && passwordLength <= 128) {
+    //     console.log(passwordLength);
+    // } else {
+    //     alert("Password must be at least 8 characters.");
+    // }
 
+    //Trying to 
+    if(passwordLength < 8 || passwordLength > 128) {
+        alert("Password must be at least 8 characters.");
+    } 
+    // else if(passwordLength > 128) {
+    //     alert("Password must be at least 8 characters.");
+    // }  
+    else (passwordLength >= 8 && passwordLength <= 128) 
+        console.log(passwordLength);
+    
     let passLower = window.confirm('Would you like to use Lower Case characters?');
 
-    console.log(typeof passLower);
     console.log(passLower);
 
     let passUpper = window.confirm('Would you like your password to include Upper Case characters?');
@@ -48,34 +57,49 @@ function generatePassword() {
     let passSym = window.confirm('Would you like to have any Special characters in your password?');
 
     console.log(passSym);
+    
+    var possChar = [','];
+    var selChar = [','];
 
-    var possChar = [];
-    var selChar = [];
-
-    if(passLower.usesLower === true) {
-       var possChars = possChars.concat(lower);
-       var selChar = lower[Math.floor(Math.random() * lower.length)];
-       possChar.push(possChars);
+    if(passLower === true) {
+        var possChars = possChar.concat(lower);
+        var selChar = lower[Math.floor(Math.random() * lower.length)];
+        possChars.push(selChar);
     }
 
-    if(passUpper.usesUpper === true) {
-        var possChars = possChars.concat(upper);
+    if(passUpper === true) {
+        var possChars = possChar.concat(upper);
         var selChar = upper[Math.floor(Math.random() * upper.length)];
-        possChar.push(possChars);
-        possChar = selChar;
+        possChars.push(selChar);
     }
 
-    if(passNum.usesNum === true) {
-        var possChars = possChars.concat(num);
+    if(passNum === true) {
+        var possChars = possChar.concat(num);
         var selChar = num[Math.floor(Math.random() * num.length)];
-        possChar.push(possChars);
+        possChars.push(selChar);
     }
 
-    if(passSym.usesSym === true) {
-        var possChars = possChars.concat(sym);
+    if(passSym === true) {
+        var possChars = possChar.concat(sym);
         var selChar = sym[Math.floor(Math.random() * sym.length)];
-        possChar.push(possChars);
+        possChars.push(selChar);
+
+        
     }
-    console.log(possChar)
+    String(possChar)
+    console.log(selChar)
+    console.log(typeof selChar)
+    console.log(typeof possChar)
+    console.log(passwordLength)
+    console.log(typeof passwordLength)
+
+
+    var password = "";
+
+    for(var i = 0; i < passwordLength; i++) {
+        var random = Math.floor(Math.random() * selChar.length);
+        password += password[random];
+    }
+    return password;
 }
 
